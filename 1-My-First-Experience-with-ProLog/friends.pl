5d09f36d842f43c2d7a8291e4e0159fa4f7a4761
Gustavo Figueira Olegário <gustavo.olegario96@gmail.com>
2016-11-11 12:58:43 -0200

    Delete friends.pl

--- 1-My-First-Experience-with-ProLog/friends.pl
+++
@@ -1,7 +0,0 @@
-friends(olegario,victor).
-friends(olegario,salomao).
-friends(salomao,victor).
-friends(salomao,caio).
-
-more_friends(FRIEND1, FRIEND3) :- friends(FRIEND1,FRIEND2),
-								  friends(FRIEND2,FRIEND3).
